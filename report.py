b1b060e2feaf60d852288c104b6990549e34b757
Roman13022003 <165912544+Roman13022003@users.noreply.github.com>
2026-02-23 10:02:03 +0200

    Revert "add average sales by city"

--- report.py
+++ report.py
@@ -5,10 +5,3 @@ df = load_data2()
 result = total_sales_by_city2(df)
 print("Загальні продажі по містах2:")
 print(result)
-
-from analysis import average_sales_by_city
-
-avg = average_sales_by_city(df)
-print("\nСередні продажі по містах:")
-print(avg)
-
